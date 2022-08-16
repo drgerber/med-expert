b2f5f84959cd17fbfe6d9cde76d5eea833350417
German Kopytov <gkopytov@yandex.ru>
2022-08-16 13:47:13 +0200

    main: сделал сплиттер компонентов рабочей области

---
+++ src/components/main/App.js
@@ -0,0 +1,19 @@
+import styles from './App.module.scss';
+import WorkFrame from "./WorkFrame";
+
+function App() {
+
+  return (    
+    <div className={styles.App}>
+      App
+      <WorkFrame
+          firstFrame={<div style={{backgroundColor: '#add7f6'}}>Hello world</div>}
+          secondFrame={<div style={{backgroundColor: '#87bfff'}}>Hello you</div>}
+          thirdFrame={<div style={{backgroundColor: '#3f8efc'}}>Hello friends</div>}
+          fourthFrame={<div style={{backgroundColor: '#2667ff'}}>Hello everyone</div>}
+      />
+    </div>
+  );
+}
+
+export default App;
